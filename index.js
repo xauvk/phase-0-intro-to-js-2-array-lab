07542cc6832b375a21ef6cc25c4de7395005bd4b
xauvk <xauvkubalexanderyang@live.com>
2022-07-06 15:47:22 -0400

    commit tree 601881c8857fe0bdc036df064397202c2485e32d

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newArray = [...cats, name]
+    return newArray
+}
+
+function prependCat(name) {
+    const newArray = [name, ...cats]
+    return newArray
+}
+
+function removeLastCat() {
+    const newArray = cats.slice(0,cats.length-1)
+    return newArray
+}
+
+function removeFirstCat() {
+    const newArray = cats.slice(1)
+    return newArray
+}
